13a69e2bc315909852b0e638b3d9704169753f26
Mashiho Ito <ito@tbp.cse.nagoya-u.ac.jp>
2016-10-17 13:15:43 +0900

    load atom information from a file

--- Argon.cpp
+++ Argon.cpp
@@ -1,4 +1,7 @@
 #include <OpenMM.h>
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
 
 void simulate();
 
@@ -11,5 +14,28 @@ void simulate() {
     // Load any shared libraries containing GPU implementations.
     OpenMM::Platform::loadPluginsFromDirectory(
         OpenMM::Platform::getDefaultPluginsDirectory());
+	
+	// Create a system with nonbonded forces.
+	OpenMM::System system;
+	OpenMM::NonbondedForce* nonbond = new OpenMM::NonbondedForce();
+	system.addForce(nonbond);
+	
+	// Create Atoms
+	const int Natoms = 3;
+	double x, y, z;
+	std::vector<OpenMM::Vec3> initPosInNm(Natoms);
 
+	// load atom information from input file.
+	std::ifstream ifs("input.dat");
+	std::string str;
+	if(ifs.fail()) {
+		std::cerr << "File do not exist.\n";
+		exit(1);
+	}
+	for (int i=0; i<Natoms; ++i) {
+		getline(ifs, str); // TODO: Exception
+		sscanf(str.data(), "%lf\t%lf\t%lf", &x, &y, &z);
+		initPosInNm[i] = OpenMM::Vec3(x, y, z);
+		std::cout << x << ',' << y << ',' << z  << std::endl;
+	}
 }
--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
-Argon:
+Argon: Argon.cpp
 	g++ Argon.cpp -o Argon -I/usr/local/openmm/include -L/usr/local/openmm/lib -lOpenMM
