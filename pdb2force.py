75fe61810f18d578da3c0b7dd385f9bb7a35ddea
Mashiho Ito <ito@tbp.cse.nagoya-u.ac.jp>
2016-10-25 10:17:33 +0900

    use OrderedDict in pdb2force for readability of JSON file

--- pdb2force.py
+++ pdb2force.py
@@ -1,9 +1,11 @@
 from PdbEditor import *
+from collections import OrderedDict
 import json
 
 mol = GoProtein("1SRL.pdb")
 
-output = {"_comment" : "This file is generated from mashmol 0.9"}
+output = OrderedDict()
+output["_comment"] = "This file is generated from mashmol 0.9"
 output["resID"] = list(mol.residues.keys())
 output["position"] = mol.positions
 output["bond"] = mol.nativeBond
@@ -11,5 +13,5 @@ output["angle"] = mol.nativeAngle
 output["dihedral"] = mol.nativeDihedral
 output["contact"] = mol.nativeContact
 
-print(json.dumps(output, indent=4, sort_keys=True))
+print(json.dumps(output, indent=4))
 
